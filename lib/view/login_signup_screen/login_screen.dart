32b2eca7ffb633ff3355db5be0813ae83849ddf9
Harshil Gajipara <harshilgajipara5@gmail.com>
2023-06-18 14:08:03 +0530

    18/6/2023

--- lib/view/login_signup_screen/login_screen.dart
+++ lib/view/login_signup_screen/login_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_typing_uninitialized_variables, use_build_context_synchronously
 
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -168,10 +169,11 @@ class _LoginPageState extends State<LoginPage> {
                       fontSize: height / 50,
                       textOnPress: () {
                         Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const PhoneNumberScreen(),
-                            ));
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const PhoneNumberScreen(),
+                          ),
+                        );
                       },
                     ),
                   ),
@@ -205,17 +207,6 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
-  }
-
-  Future<String> signUp(String username, String email, String password) async {
-    FirebaseUser user = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
-    try {
-      await user.sendEmailVerification();
-      return user.uid;
-    } catch (e) {
-      print("An error occured while trying to send email        verification");
-      print(e.message);
-    }
   }
 
   Future<void> signInWithGoogle() async {
